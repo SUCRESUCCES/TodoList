f47ca643f94e2bd18c341c870d55d189209b91b1
밍 <sucresucces@icloud.com>
2025-08-19 21:52:03 +0900

    Create(투두 추가하기)
    1. 자동 포커스
    - 화면 처음 렌더링시 입력창에 커서 자동 이동
    -  Todo 추가 후에도 다시 커서 유지
    
    2. Todo 입력 처리
    -  input에 글자 입력시 content 상태 관리
    - Enter 또는 버튼 클릭으로 Todo 추가 가능
    
    3. 상위 상태 업데이트
    - onCreate(content) 실행 시 새로운 Todo 객체 생성
    - todos 상태를 업데이트해서 화면에 즉시 반영

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Header from "@/components/Header";
 import Editor from "@/components/Editor";
 import List from "@/components/List";
@@ -9,11 +9,22 @@ import { Todo } from "@/types/todo";
 
 export default function Home() {
   const [todos, setTodos] = useState<Todo[]>(mockTodos);
+  const idRef = useRef<number>(3);
+
+  const onCreate = (content: string) => {
+    const newTodo: Todo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      createdAt: new Date().getTime(),
+    };
+    setTodos((prevTodos) => [newTodo, ...prevTodos]);
+  };
 
   return (
     <main className="animate-fade-in w-full max-w-lg mx-auto flex flex-col gap-4 p-4">
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate} />
       <List todos={todos} />
     </main>
   );
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,7 +1,45 @@
-const Editor = () => {
+import { useState, useRef, useEffect } from "react";
+import React from "react";
+
+interface EditorProps {
+  onCreate: (content: string) => void;
+}
+
+const Editor = ({ onCreate }: EditorProps) => {
+  const [content, setContent] = useState<string>("");
+  const contentRef = useRef<HTMLInputElement>(null);
+
+  // 첫 렌더링 시 자동 포커스
+  useEffect(() => {
+    contentRef.current?.focus();
+  });
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setContent(e.target.value);
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter") {
+      onsubmit();
+    }
+  };
+
+  const onsubmit = () => {
+    if (content === "") {
+      contentRef.current?.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
+    contentRef.current?.focus();
+  };
   return (
     <div className="flex gap-2.5 px-2">
       <input
+        ref={contentRef}
+        value={content}
+        onKeyDown={onKeyDown}
+        onChange={onChange}
         placeholder="새로운 Todo..."
         className="flex-1 px-4
         py-3
@@ -14,6 +52,7 @@ const Editor = () => {
         sm:placeholder:text-sm"
       />
       <button
+        onClick={onsubmit}
         className="
       w-20  
       rounded 
